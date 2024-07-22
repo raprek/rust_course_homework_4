ffb7496dfc3c24863dc89e4e84ae0b85d7d448f6
Anton Anisimov <raprek1@gmail.com>
2024-07-22 20:16:49 +0300

    -> add | dirty

---
+++ src/collections/counter.rs
@@ -0,0 +1,77 @@
+pub struct SignedCounter  {
+    value: isize,
+}
+
+pub struct UnsignedCounter  {
+    value: isize,
+}
+
+impl SignedCounter {
+    pub fn new(value: isize) -> Self {
+        Self { value }
+    }
+
+    pub fn prev_signed(self: Self) -> Self {
+        Self {value: self.value - 1}
+    }
+
+    pub fn next_signed(self: Self) -> Self {
+        Self {value: self.value + 1}
+    }
+
+
+}
+
+impl Default for SignedCounter {
+    fn default() -> Self {
+        Self { value: 0 }
+    }
+}
+
+impl UnsignedCounter {
+    pub fn next_unsigned(self: Self) -> Self {
+        Self {value: self.value + 1}
+    }
+
+    pub fn default() -> Self {
+        Self { value: 0 }
+    }
+}
+
+impl Default for UnsignedCounter {
+    fn default() -> Self {
+        Self { value: 0 }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_default_signed_counter() {
+        assert_eq!(SignedCounter::default().value, 0);
+    }
+
+    #[test]
+    fn test_default_unsigned_counter() {
+        assert_eq!(UnsignedCounter::default().value, 0);
+    }
+
+    #[test]
+    fn test_prev_signed() {
+        let counter: SignedCounter = SignedCounter { value: 1 };
+        assert_eq!(counter.prev_signed().value, 0);
+    }
+
+    #[test]
+    fn test_next_signed() {
+        let counter: SignedCounter = SignedCounter {value: 1};
+        assert_eq!(counter.prev_signed().value, 0);
+    }
+
+    #[test]
+    fn test_next_unsigned() {
+        let counter: UnsignedCounter = UnsignedCounter { value: 1 };
+        assert_eq!(counter.next_unsigned().value, 2)
+    }
+}
---
+++ src/collections/vector.rs
@@ -0,0 +1,52 @@
+pub const VEC3_LEN: usize = 3;
+pub struct Vec3 {
+    value: [i32; VEC3_LEN], 
+}
+
+impl Vec3 {
+    pub fn vec3_vector_sum(a: Vec3, b: Vec3) -> Vec3 {
+        let mut c = Vec3::default();
+        for i in 0..3 {
+            c.value[i] = a.value[i] + b.value[i];
+        }
+        c
+    }
+    
+    pub fn vec3_scalar_sum(a: Vec3, b: Vec3) -> i32 {
+        let mut c = 0;
+        for i in 0..VEC3_LEN {
+            c += a.value[i] + b.value[i];
+        }
+        c
+    }
+
+}
+
+impl Default for Vec3 {
+    fn default() -> Self {
+        Vec3{value: [0; 3]}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_vec3_vector_sum() {
+        let ref_test = Vec3::vec3_vector_sum(Vec3{value: [1, 2, 3]}, Vec3{value:[3, 2, 1]});
+        assert_eq!(ref_test.value[0], 4);
+        assert_eq!(ref_test.value[1], 4);
+        assert_eq!(ref_test.value[2], 4);
+    }
+
+    #[test]
+    fn test_vec3_scalar_sum() {
+        assert_eq!(Vec3::vec3_scalar_sum(Vec3{value:[1, 2, 3]}, Vec3{value:[3, 2, 1]}), 12)
+    }
+
+    #[test]
+    fn test_default_vec3() {
+        assert_eq!(Vec3::default().value, [0; 3]);
+    }
+}
